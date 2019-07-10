f88f114ca9bae195c3c43f0a3f514c9343a5bc7b
Irma Mejia <ijanettmejia@outlook.com>
2019-07-09 21:17:42 -0400

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,21 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "e86ecddf4fd803e692ec4d19a8d49090e2cf54dd"
+    set :session_secret, "e86ecddf4fd803e692ec4d19a8d49090e2cf54dd" #generated with ruby -e "require 'securerandom'; puts SecureRandom.hex(20)"
   end
 
   get "/" do
     erb :index
   end
+
+  helpers do
+    def current_user
+      Guest.find(session[:guest_id])
+    end
+
+    def logged_in?
+      !!session[:guest_id]
+    end
+  end
+  
 end
