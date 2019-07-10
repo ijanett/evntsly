bd742f4cadc314cc0d333f8a219618d65f40853d
Irma Mejia <ijanettmejia@outlook.com>
2019-07-10 13:25:53 -0400

    Seed the database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
     get '/events' do
         if logged_in?
             @events = Event.all
-
+# binding.pry
             erb :'/events/home'
         else
             redirect to '/login'
