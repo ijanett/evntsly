5afb172b9f3e5aee7b0b7fbe182aeeaeb52a4bbe
Irma Mejia <ijanettmejia@outlook.com>
2019-07-13 03:05:41 -0400

    Add image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "e86ecddf4fd803e692ec4d19a8d49090e2cf54dd" #generated with ruby -e "require 'securerandom'; puts SecureRandom.hex(20)"
+    set :session_secret, "evntsly_secret"
   end
 
   get "/" do
--- app/controllers/event_guests_controller.rb
+++ app/controllers/event_guests_controller.rb
@@ -3,7 +3,7 @@ class EventGuestsController < ApplicationController
     post '/event_guests' do
         EventGuest.find_or_create_by(params[:event_guest])
         guest = Guest.find(params[:event_guest][:guest_id])
-
+        
         redirect "/#{guest.username}"
     end
 
